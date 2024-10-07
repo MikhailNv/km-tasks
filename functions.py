803a321f835a99a59fd5a99d776d8e19c5b11e4e
Mikhail Naumov <mihail20022015@gmail.com>
2024-10-07 23:58:37 +0300

    Обновил код задания

--- functions.py
+++ functions.py
@@ -1,67 +1,87 @@
 import os
+from glob import glob, iglob
 import numpy as np
 import pandas as pd
 import cv2
 
 
-def extract_ppg_signal(frame_folder_path):
-    frame_files = sorted(
-        [f for f in os.listdir(frame_folder_path) if f.endswith((".jpg", ".png"))]
-    )
-    red_channel = []
-    green_channel = []
-    blue_channel = []
-    for frame_file in frame_files:
-        frame_path = os.path.join(frame_folder_path, frame_file)
-        frame = cv2.imread(frame_path)
-        if frame is None:
-            continue
-        # BGR
-        b_mean = np.mean(frame[:, :, 0])
-        g_mean = np.mean(frame[:, :, 1])
-        r_mean = np.mean(frame[:, :, 2])
-        blue_channel.append(b_mean)
-        green_channel.append(g_mean)
-        red_channel.append(r_mean)
-    # Take average of the 3 channels
-    ppg_signal = (
-        np.array(red_channel) + np.array(green_channel) + np.array(blue_channel)
-    ) / 3
-    return ppg_signal
+__all__ = [
+    "Data2PPG"
+]
 
 
-def get_windows_and_labels(ppg_signal, gt_df, window_sec=5, fps=25):
-    count_gt = len(gt_df.index)
-    count_windows = count_gt // window_sec
-    windows = []
-    labels = []
-    window_size = window_sec * fps
-    for i in range(count_windows):
-        start_idx = i * window_size
-        end_idx = start_idx + window_size
-        window = ppg_signal[start_idx:end_idx]
-        windows.append(window)
-        # Get ground truth for the window
-        gt_start = i * window_sec
-        gt_end = gt_start + window_sec - 1
-        gt_values = gt_df.loc[gt_start:gt_end, "SpO2"]
-        label = np.mean(gt_values)  # truth value for this window
-        labels.append(label)
-    return windows, labels
+class Data2PPG:
+    @classmethod
+    def __get_ppg_signal(cls, images_path: str):
+        """ PPG Signal Acquisition Method
+        Args:
+            images_path: str - Images generator object
+        Returns:
+            ppg_signal: NDArray - PPG signal array
+        """
+        rc = []
+        gc = []
+        bc = []
+        for frame_file in images_path:
+            frame = cv2.imread(frame_file)
+            if frame is None:
+                continue
+            b_mean = np.mean(frame[:, :, 0])
+            g_mean = np.mean(frame[:, :, 1])
+            r_mean = np.mean(frame[:, :, 2])
+            rc.append(r_mean)
+            gc.append(g_mean)
+            bc.append(b_mean)
+        ppg_signal = (
+            np.array(rc) + np.array(gc) + np.array(bc)
+        ) / 3
+        return ppg_signal
 
+    @classmethod
+    def __get_windows_and_labels(cls, ppg_signal, csv_df, window_sec=5, fps=25):
+        """ Method of receiving signals of different time ranges
+        Args:
+            ppg_signal: NDArray - PPG signal array
+            csv_df: DataFrame - SpO2 each subject DataFrame
+            window_sec: int - window size
+            fps: int - Frames Per Second 
+        Returns:
+            (windows, labels): Tuple - Tuple of found windows and labels
+        """
+        count_gt = len(csv_df.index)
+        count_windows = count_gt // window_sec
+        windows = []
+        labels = []
+        window_size = window_sec * fps
+        for window_idx in range(count_windows):
+            start_idx = window_idx * window_size
+            end_idx = start_idx + window_size
+            window = ppg_signal[start_idx:end_idx]
+            windows.append(window)
+            gt_start = window_idx * window_sec
+            gt_end = gt_start + window_sec - 1
+            gt_values = csv_df.loc[gt_start:gt_end, "SpO2"]
+            label = np.mean(gt_values)
+            labels.append(label)
+        return windows, labels
 
-def load_data(data_path):
-    X = []  # input values in CNN
-    y = []  # truth output values for CNN
-    subjects = os.listdir(data_path)
-    for subject in subjects:
-        face_path = os.path.join(data_path, subject, "Face")
-        gt_path = os.path.join(data_path, subject, "gt_SpO2.csv")
-        gt_df = pd.read_csv(gt_path)  # dataframe with SpO2 values
-        # Extract PPG signal
-        ppg_signal = extract_ppg_signal(face_path)
-        # Break down signals into windows
-        windows, labels = get_windows_and_labels(ppg_signal, gt_df)
-        X.extend(windows)
-        y.extend(labels)
-    return np.array(X), np.array(y)
+    @classmethod
+    def get_metrics(cls, data_path: str):
+        """ Method of receiving X and y metrics
+        Args:
+            data_path: str - Path to Train/Validate/Test data
+        Returns:
+            (X, y): Tuple - Tuple of found metrics
+        """
+        X_values = []
+        y_values = []
+        subjects = os.listdir(data_path)
+        for subject in subjects:
+            images_path = glob(f"{data_path}/{subject}/**/*.jpg")
+            csv_path = glob(f"{data_path}/{subject}/gt_SpO2.csv").pop()
+            csv_df = pd.read_csv(csv_path)
+            ppg_signal = cls.__get_ppg_signal(images_path)
+            windows, labels = cls.__get_windows_and_labels(ppg_signal, csv_df)
+            X_values.extend(windows)
+            y_values.extend(labels)
+        return np.array(X_values), np.array(y_values)
--- task_3.py
+++ task_3.py
@@ -1,59 +1,77 @@
 import numpy as np
 import xgboost as xgb
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import GridSearchCV, train_test_split, cross_val_score, RepeatedKFold
-from functions import load_data
-import pandas as pd
+from sklearn.model_selection import GridSearchCV, cross_val_score, RepeatedKFold
+from functions import Data2PPG #load_data
 import matplotlib.pyplot as plt
 
-# Paths to the data directories
-train_path = "Task/Train"
-validation_path = "Task/Validation"
-test_path = "Task/Test"
+train_path = "Task\Train"
+validation_path = "Task\Validation"
+test_path = "Task\Test"
 
 print("Loading training data...")
-X_train, y_train = load_data(train_path)  # Используем длину сигнала 5 секунд
+X_train, y_train = Data2PPG.get_metrics(train_path)
 print("Loading validation data...")
-X_val, y_val = load_data(validation_path)
-print("Loading test data...")
-X_test, y_test = load_data(test_path)
+X_val, y_val = Data2PPG.get_metrics(validation_path)
+print("Loading testing data...")
+X_test, y_test = Data2PPG.get_metrics(test_path)
 
-# Инициализация XGBoost регрессора
+# Initializing XGBoost regressor
 xgb_reg = xgb.XGBRegressor(objective='reg:squarederror')
 
-# define model evaluation method
+# Define model evaluation method
 cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
-# evaluate model
-scores = cross_val_score(xgb_reg, X_train, y_train, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1)
-# force scores to be positive
+# Evaluate model
+scores = cross_val_score(
+    xgb_reg,
+    X_train,
+    y_train,
+    scoring='neg_mean_absolute_error',
+    cv=cv,
+    n_jobs=-1
+)
+# Force scores to be positive
 scores = np.absolute(scores)
 print('Mean MAE: %.3f (%.3f)' % (scores.mean(), scores.std()) )
 
-# Определение сетки гиперпараметров
+# Defining a Hyperparameter Mesh
 param_grid = {
-    'n_estimators': [100, 300, 700, 1000],  # Количество деревьев
-    'max_depth': [5, 7, 9, 11],            # Максимальная глубина деревьев
+    'n_estimators': [100, 300, 700, 1000],  # Number of trees
+    'max_depth': [5, 7, 9, 11],             # Maximum Tree Depth
 }
 
-# Grid Search с кросс-валидацией
-grid_search = GridSearchCV(estimator=xgb_reg, param_grid=param_grid, cv=cv, scoring='neg_mean_squared_error', verbose=1)
+# Grid Search with cross-validation
+grid_search = GridSearchCV(
+    estimator=xgb_reg,
+    param_grid=param_grid,
+    cv=cv,
+    scoring='neg_mean_squared_error',
+    verbose=1
+)
 
-# Обучение модели на тренировочных данных
+# Train the model on training data
 grid_search.fit(X_train, y_train)
 
+# Get best params
 print("BEST_PARAMS: ", grid_search.best_params_)
-print(grid_search.best_score_)
 
+# Predict the model on testing data
 y_pred = grid_search.predict(X_test)
+# Get MSE on testing and training metrics
 mse = mean_squared_error(y_test, y_pred)
-rmse = r2_score(y_test, y_pred)
-
+# Get RMSE on testing and training metrics
+rmse = np.sqrt(mse)
 print("MSE: ", mse)
 print("RMSE: ", rmse)
+
+xgb.plot_tree(grid_search.best_estimator_.get_booster())
+plt.title("Our XGBoost tree with Graphviz")
+plt.show()
 
 x_ax = range(len(y_test))
 plt.plot(x_ax, y_test, label="original")
 plt.plot(x_ax, y_pred, label="predicted")
 plt.title("Predicting data")
+plt.text(0, 87, f'MAE: {scores.mean()}\nMSE: {mse}\nRMSE: {rmse}\nBest Params: {grid_search.best_params_}')
 plt.legend()
 plt.show()
